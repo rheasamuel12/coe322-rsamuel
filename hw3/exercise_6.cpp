ebefc95d0b0ccdb56dd7a05233bbf6dabddad1ba
rheasamuel <rheasamuel@isp02.tacc.utexas.edu>
2023-09-05 15:49:09 -0500

    HW3E6 With comments

--- hw3/exercise_6.cpp
+++ hw3/exercise_6.cpp
@@ -3,28 +3,28 @@
 using namespace std;
 //rhea samuel
 //rss3488
-void createVect(vector<int> vect)
+void createVect(vector<int> vect) //function
 {
-    vector <int> odd;
-    vector <int> even;
+    vector <int> odd; // odd vector
+    vector <int> even; // even vector
     for(int x = 0; x<vect.size(); x++)
     {
         if(vect[x] %2 == 0)
         {
-            even.push_back(vect[x]);
+            even.push_back(vect[x]); //pushes even values to new vector
         }
         else{
-            odd.push_back(vect[x]);
+            odd.push_back(vect[x]); //pushes odd values to new vector
         }
     }
     for(int x = 0; x<odd.size(); x++)
     {
         if(x!=odd.size()-1)
         {
-            cout << odd[x] << ",";
+            cout << odd[x] << ","; //prints out odd #s
         }
         else{
-            cout << odd[x];
+            cout << odd[x]; //last number with no comma
         }
     }
     cout << endl;
@@ -32,17 +32,17 @@ void createVect(vector<int> vect)
     {
         if(x!=even.size()-1)
         {
-            cout << even[x] << ",";
+            cout << even[x] << ","; //prints out even #s
         }
         else{
-            cout << even[x];
+            cout << even[x]; //last number with no comma
         }
     }
     cout << endl;
 };
 int main()
 {
-    vector <int> input = {5,6,2,4,5};
+    vector <int> input = {5,6,2,4,5}; //input
     createVect(input);
 
 }
