d45dc5c83daa8f3ba3585ae4d2ca85906fd37ee7
rheasamuel <rheasamuel@isp02.tacc.utexas.edu>
2023-11-25 00:31:40 -0600

    24

--- finalproject/infectiousdisease.hpp
+++ finalproject/infectiousdisease.hpp
@@ -11,23 +11,37 @@ using namespace std;
 
 class Disease{
     public:
-    double chance_transmission;
-    int infectious_days;
+    double transmission_probability;
+    int duration;
 
-    Disease(int transmissionChance, int daysSick)
-        :chance_transmission(transmissionChance), infectious_days(daysSick) {}
+    //exercise 49.2
+    Disease(int daysSick, int transmissionChance) 
+        :duration(daysSick) , transmission_probability(transmissionChance){}
 
-    int getInfectiousDays() const
+    Disease(){} //empty constructor
+
+    //exercise 49.3
+    void setDuration(int days)
     {
-        return infectious_days;
+        duration = days;
+    }
+    //exercise 49.3
+    void setTransmissionChance(double prob)
+    {
+        transmission_probability = prob;
+    }
+
+    int getDuration() const
+    {
+        return duration;
     }
     double getTransmissionChance() const
     {
-        return chance_transmission;
+        return transmission_probability;
     }
 };
 
-
+//exercise 49.1
 class Person{
     private:
     string status; //status of person (susceptible, infected, recovered, vaccinated)
@@ -39,7 +53,7 @@ class Person{
     void infect(const Disease& disease) {
         if (status == "Susceptible") {
             status = "Infected";
-            infectiousDays = disease.getInfectiousDays();
+            infectiousDays = disease.getDuration();
         }
         
      }
@@ -48,14 +62,13 @@ class Person{
         return status;
     }
     void one_more_day(){ //updates the status of the person to the next day
-       if(status == "Infected")
+       if(status == "Infected" && infectiousDays>0)
        {
-            if(!isRecovered())
-            {
-                infectiousDays--;
-            }
-            
+            infectiousDays--;
        }
+       else{
+            isRecovered();
+        }
     }
     bool isRecovered(){
         if(infectiousDays<=0)
@@ -74,11 +87,25 @@ class Person{
         infectiousDays = 0;
     }
 
+    void touch(Person& infected, Disease& disease){
+        if (status == "Susceptible" && infected.get_status() == "Infected") {
+            double randomValue = static_cast<double>(rand()) / RAND_MAX; // Random value between 0 and 1
+            if (randomValue < disease.getTransmissionChance()) {
+                infect(disease);
+            }
+        }
+    }
+
+
+};
+
+class Population{
+
 };
 
 /*
 int main(){
-    Disease covid(0.5, 5);  // disease with 50% transmission chance and 5 days of sickness
+    Disease covid(5, 0.5);  // disease with 50% transmission chance and 5 days of sickness
     Person Rhea;
 
     // On each day, simulate Rhea's progression
