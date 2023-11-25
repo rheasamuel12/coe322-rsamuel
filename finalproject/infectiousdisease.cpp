165f7f172cd9c64707c3df25aa76d6e31c0682af
Rhea Samuel <rheasamuel@isp02.tacc.utexas.edu>
2023-11-25 00:37:42 -0600

    commit

---
+++ finalproject/infectiousdisease.cpp
@@ -0,0 +1,137 @@
+// Name: Rhea Samuel
+// UTEID:rss3488
+// TACC Username: rheasamuel12 
+//Final Project: Infectious Disease Simulation
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+#include "infectiousdisease.hpp"
+#define CATCH_CONFIG_MAIN
+#include "catch2/catch_all.hpp"
+
+
+
+
+TEST_CASE("Infection with 100% transmittable disease", "[Person]") {
+    Disease covid(1.0, 5); // 100% transmission chance, 5 days of infection
+    Person person;
+    // After being infected, the person should register as sick
+    person.infect(covid);
+    REQUIRE(person.get_status() == "Infected");
+}
+
+TEST_CASE("Contact with vaccinated/recovered person and disease", "[Person]") {
+    Disease covid(0.5, 7); // 50% transmission chance
+    Person vaccinatedPerson("Vaccinated", 0);
+    //vaccinatedPerson.get_vaccinated();
+
+    Person recoveredPerson("Recovered", 0);
+    //recoveredPerson.one_more_day(); // Make the person "Recovered"
+
+    // If vaccinated or recovered, they should stay in their original state
+    vaccinatedPerson.infect(covid);
+    recoveredPerson.infect(covid);
+
+    REQUIRE(vaccinatedPerson.get_status() == "Vaccinated");
+    REQUIRE(recoveredPerson.get_status() == "Recovered");
+}
+
+TEST_CASE("Infection Spread Test", "[Infection]") {
+    // Create a disease with a 50% transmission chance and 5 days of sickness
+    Disease covid(5, 0.5);
+
+    // Create a population of people
+    const int populationSize = 1000; // Adjust as needed
+    vector<Person> people(populationSize);
+
+    // Infect a fixed percentage of people (e.g., 50%)
+    const int infectedPercentage = covid.getTransmissionChance()*100;
+    const int numInfected = (populationSize * infectedPercentage) / 100;
+
+    for (int i = 0; i < numInfected; ++i) {
+        int randomIndex = rand() % populationSize;
+        people[randomIndex].infect(covid);
+    }
+
+/*
+    // Simulate the progression for a number of days
+    const int simulationDays = 10; // Adjust as needed
+
+    for (int day = 1; day <= simulationDays; ++day) {
+        // Simulate one more day for each person in the population
+        for (auto& person : people) {
+            person.one_more_day();
+        }
+    }
+*/
+    // Count the number of people who are sick after the simulation
+    int numSick = 0;
+    for (const auto& person : people) {
+        if (person.get_status() == "Infected" || person.get_status() == "Recovered") {
+            numSick++;
+        }
+    }
+
+    // Check if about half of the people got sick
+    double actualPercentage = static_cast<double>(numSick) / populationSize;
+
+    // Allow some tolerance due to randomness
+    double expectedPercentage = static_cast<double>(infectedPercentage) / 100;
+    REQUIRE(actualPercentage >= expectedPercentage - 0.1);
+    REQUIRE(actualPercentage <= expectedPercentage + 0.1);
+}
+
+
+
+//Adapt the above tests, but now a person comes in contact with an infected person, rather than directly
+//with a disease
+
+TEST_CASE("Infection Spread Test", "[Infection]") {
+    // Create a disease with a 50% transmission chance and 5 days of sickness
+    Disease covid(5, 0.5);
+
+    // Create a population of people
+    const int populationSize = 1000; // Adjust as needed
+    std::vector<Person> people(populationSize);
+
+    // Infect a fixed percentage of people (e.g., 50%)
+    const int infectedPercentage = 50;
+    const int numInfected = (populationSize * infectedPercentage) / 100;
+
+    for (int i = 0; i < numInfected; ++i) {
+        int randomIndex = rand() % populationSize;
+        people[randomIndex].infect(covid);
+    }
+
+    // Simulate the progression for a number of days
+    const int simulationDays = 5; // Adjust as needed
+
+    for (int day = 1; day <= simulationDays; ++day) {
+        // Simulate one more day for each person in the population
+        for (auto& person : people) {
+            person.one_more_day();
+
+            // Check if the person can infect others by coming in contact with infected individuals
+            for (auto& otherPerson : people) {
+                person.touch(otherPerson, covid);
+            }
+        }
+    }
+
+    // Count the number of people who are sick after the simulation
+    int numSick = 0;
+    for (const auto& person : people) {
+        if (person.get_status() == "Infected") {
+            numSick++;
+        }
+    }
+
+    // Check if about half of the people got sick
+    double actualPercentage = static_cast<double>(numSick) / populationSize;
+
+    // Allow some tolerance due to randomness
+    double expectedPercentage = static_cast<double>(infectedPercentage) / 100;
+    REQUIRE(actualPercentage >= expectedPercentage - 0.1);
+    REQUIRE(actualPercentage <= expectedPercentage + 0.1);
+}
