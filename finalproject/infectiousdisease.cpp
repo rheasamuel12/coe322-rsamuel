b9df96b6ba3a85f2ed07badbc23cf267b69f3965
Rhea Samuel <rheasamuel@isp02.tacc.utexas.edu>
2023-11-25 15:34:03 -0600

    m

--- finalproject/infectiousdisease.cpp
+++ finalproject/infectiousdisease.cpp
@@ -78,32 +78,18 @@ TEST_CASE("Infection Spread Test", "[Infection]") {
 
 TEST_CASE("Infection Spread Test 2", "[Infect]") {
     // Create a disease with a 50% transmission chance and 5 days of sickness
-    Disease covid(0.5,5);
+    Disease covid(5,0.5);
 
     // Create a population of people
     const int populationSize = 1000; // Adjust as needed
     vector<Person> people(populationSize);
-
-    // Infect a fixed percentage of people (e.g., 50%)
-
-   for(int i = 0; i<populationSize; i++){
-        double randomValue = static_cast<double>(rand()) / (RAND_MAX + 1.0);
-        if(randomValue < covid.getTransmissionChance())
-        {
-            people[i].infect(covid);
-        }
-    }
+    Person infected;
+    infected.infect(covid);
     // Simulate the progression for a number of days
-    const int simulationDays = 5; // Adjust as needed
-
-    for (int day = 1; day <= simulationDays; ++day) {
-        // Simulate one more day for each person in the population
-        for (auto& person : people) {
-            // Check if the person can infect others by coming in contact with infected individuals
-            for (auto& otherPerson : people) {
-                person.touch(otherPerson, covid);
-            }
-        }
+    // Check if the person can infect others by coming in contact with infected individuals
+    for(int x = 0; x<populationSize; x++)
+    {
+        infected.touch(people[x],covid);
     }
 
     // Count the number of people who are sick after the simulation
@@ -116,7 +102,6 @@ TEST_CASE("Infection Spread Test 2", "[Infect]") {
 
     // Check if about half of the people got sick
     double actualPercentage = static_cast<double>(numSick) / populationSize;
-
     // Allow some tolerance due to randomness
     REQUIRE(actualPercentage >= covid.getTransmissionChance() - 0.1);
     REQUIRE(actualPercentage <= covid.getTransmissionChance() + 0.1);
