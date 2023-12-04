1a86958799836ac84302e9a42c992467e24ec195
Rhea Samuel <rheasamuel@isp02.tacc.utexas.edu>
2023-12-03 20:36:16 -0600

    m

--- finalproject/infectiousdisease.cpp
+++ finalproject/infectiousdisease.cpp
@@ -15,15 +15,15 @@ using namespace std;
 
 //49.2.1.1 Person tests
 TEST_CASE("Infection with 100% transmittable disease", "[Person]") {
-    Disease covid(5,1.0); // 100% transmission chance, 5 days of infection
+    Disease disease(5,1.0); // 100% transmission chance, 5 days of infection
     Person person;
     // After being infected, the person should register as sick
-    person.infect(covid);
+    person.infect(disease);
     REQUIRE(person.get_status() == "Infected");
 }
 
 TEST_CASE("Contact with vaccinated/recovered person and disease", "[Person]") {
-    Disease covid(7,1.0); // 50% transmission chance
+    Disease disease(7,1.0); // 50% transmission chance
     Person vaccinatedPerson("Vaccinated", 0);
     //vaccinatedPerson.get_vaccinated();
 
@@ -31,8 +31,8 @@ TEST_CASE("Contact with vaccinated/recovered person and disease", "[Person]") {
     //recoveredPerson.one_more_day(); // Make the person "Recovered"
 
     // If vaccinated or recovered, they should stay in their original state
-    vaccinatedPerson.infect(covid);
-    recoveredPerson.infect(covid);
+    vaccinatedPerson.infect(disease);
+    recoveredPerson.infect(disease);
 
     REQUIRE(vaccinatedPerson.get_status() == "Vaccinated");
     REQUIRE(recoveredPerson.get_status() == "Recovered");
@@ -40,18 +40,18 @@ TEST_CASE("Contact with vaccinated/recovered person and disease", "[Person]") {
 
 TEST_CASE("Infection Spread Test", "[Infection]") {
     // Create a disease with a 50% transmission chance and 5 days of sickness
-    Disease covid(5,0.5);
+    Disease disease(5,0.5);
 
     // Create a population of people
-    const int populationSize = 1000; // Adjust as needed
+    const int populationSize = 100000; // Adjust as needed
     vector<Person> people(populationSize);
 
     // Infect a fixed percentage of people (e.g., 50%)
     for(int i = 0; i<populationSize; i++){
         double randomValue = static_cast<double>(rand()) / (RAND_MAX + 1.0);
-        if(randomValue < covid.getTransmissionChance())
+        if(randomValue < disease.getTransmissionChance())
         {
-            people[i].infect(covid);
+            people[i].infect(disease);
         }
     }
 
@@ -79,18 +79,18 @@ TEST_CASE("Infection Spread Test", "[Infection]") {
 //49.2.2.1 Interaction tests
 TEST_CASE("Infection Spread Test 2", "[Infect]") {
     // Create a disease with a 50% transmission chance and 5 days of sickness
-    Disease covid(5,0.5);
+    Disease disease(5,0.5);
 
     // Create a population of people
-    const int populationSize = 1000; // Adjust as needed
+    const int populationSize = 100000; // Adjust as needed
     vector<Person> people(populationSize);
     Person infected;
-    infected.infect(covid);
+    infected.infect(disease);
     // Simulate the progression for a number of days
     // Check if the person can infect others by coming in contact with infected individuals
     for(int x = 0; x<populationSize; x++)
     {
-        infected.touch(people[x],covid);
+        infected.touch(people[x],disease);
     }
 
     // Count the number of people who are sick after the simulation
@@ -111,10 +111,10 @@ TEST_CASE("Infection Spread Test 2", "[Infect]") {
 //49.2.3.1 POPULATION TESTS
 TEST_CASE("Everyone is vaccinated with 100 vacccination rate", "[vaccination]") {
     // Create a Disease with 0% transmission chance and 5 days of sickness
-    Disease covid(5, 0.0);
+    Disease disease(5, 0.0);
 
     // Create a Population with 10 people and 100% vaccination rate
-    Population population(1000, covid);
+    Population population(100000, disease);
     population.random_vaccination(1.0);
 
     // Check if everyone is vaccinated
@@ -125,10 +125,10 @@ TEST_CASE("Everyone is vaccinated with 100 vacccination rate", "[vaccination]") 
 //4.9.3.1.1 Tests
 TEST_CASE("Duration of disease", "[infection][population]") {
         // Create a Disease with 50% transmission chance and 5 days of sickness
-        Disease covid(5, 0.5);
+        Disease disease(5, 0.5);
 
         // Create a Population with 100 people and 0% vaccination rate
-        Population population(100, covid);
+        Population population(100000, disease);
 
         int initialInfected = population.count_infected();
         int initialHealthy = population.populationSize - initialInfected;
@@ -136,7 +136,7 @@ TEST_CASE("Duration of disease", "[infection][population]") {
         // Run the simulation for the duration of the disease
         int currentInfected = 0;
         int currentHealthy = 0;
-        for (int day = 1; day <= covid.getDuration()-1; ++day) {
+        for (int day = 1; day <= disease.getDuration()-1; ++day) {
             population.one_more_day();
             currentInfected = population.count_infected();
             currentHealthy = population.populationSize - currentInfected;
@@ -153,9 +153,9 @@ TEST_CASE("Duration of disease", "[infection][population]") {
 //49.3.2.1 sanity tests 
 TEST_CASE("Test simulation with p = 1", "[simulation]") {
     //tests if random index is sick, next day 3 are sick
-    Disease covid(5, 1);
-    Population population(10, covid);
-    population.initial_infect(covid);
+    Disease disease(5, 1);
+    Population population(100000, disease);
+    population.initial_infect(disease);
     //one day
     int day = 0;
     bool ret = false;
@@ -164,7 +164,7 @@ TEST_CASE("Test simulation with p = 1", "[simulation]") {
     }
     while(day<1){
         population.one_more_day();
-        population.neighbor(covid, 1);
+        population.neighbor(disease, 1);
         day++;
     }
     if(ret)
@@ -176,21 +176,21 @@ TEST_CASE("Test simulation with p = 1", "[simulation]") {
 }
 //if index is 0, then the simulation should run for the number of days equal to population size
 TEST_CASE("Test simulation with p = 1, Index = 0", "[simulation]") {
-    Disease covid(5, 1);
-    Population population(10, covid);
-    population.people[0].infect(covid);
+    Disease disease(5, 1);
+    Population population(100000, disease);
+    population.people[0].infect(disease);
     int days = 1;
     while(population.people[population.populationSize-1].get_status() == "Susceptible"){        
         population.one_more_day();
-        population.neighbor(covid, 1); 
+        population.neighbor(disease, 1); 
         days++;
     }
     REQUIRE(days == population.populationSize); 
 }
 TEST_CASE("Test simulation with p = 0.5", "[simulation]") {
-    Disease covid(5, 0.5);
-    Population population(10, covid);
-    population.people[0].infect(covid);
+    Disease disease(5, 0.5);
+    Population population(100000, disease);
+    population.people[0].infect(disease);
     int days = 1;
     int countInfected = 1;
     while(countInfected>0){
@@ -198,7 +198,7 @@ TEST_CASE("Test simulation with p = 0.5", "[simulation]") {
             break;
         countInfected = population.count_infected();
         population.one_more_day();
-        population.neighbor(covid, 0.5); 
+        population.neighbor(disease, 0.5); 
         days++;
     }
 
