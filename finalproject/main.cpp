c4cc05aa09dc8753b3f03036d56186f96aa7d319
Rhea Samuel <rheasamuel@isp02.tacc.utexas.edu>
2023-12-04 17:34:06 -0600

    Main 1 for infectiousdisease.hpp

--- finalproject/main.cpp
+++ finalproject/main.cpp
@@ -3,15 +3,15 @@
 using namespace std;
 
 //exercise 49.1
-void personSimulation(){
+    void personSimulation(){
         Person Rhea;
         Disease disease(5);  
-        for (int x = 1; x <= 10; x++) {
+        for (int x = 1; x <= 10; x++) { //loops through 10 days 
             Rhea.one_more_day();
-            if(Rhea.get_status() == "Infected"){
+            if(Rhea.get_status() == "Infected"){ //Displays my infectious days
                 cout << "On day " << x << ", Rhea is " << Rhea.get_status() << " (" << Rhea.get_infectiousDays() << " more days to go)" << endl;
             }
-            else
+            else //returns my status
                 cout << "On day " << x << ", Rhea is " << Rhea.get_status() << endl;
             if (x == 3) {
                 Rhea.infect(disease);
@@ -21,49 +21,49 @@ void personSimulation(){
       
         }
     }
-
-
-    //exercise 49.4
+//exercise 49.4
     void populationSimulation(int popSize, double probability){
         Disease disease(5, probability);  
         Population population(popSize,disease);
-        //population.initial_infect(disease); //infect only one person initially
-        population.random_infectious(0.01, disease); //infect 10% of the population
+        population.random_infectious(0.01, disease); //infect 1% of the population
         int countInfected =0;
         int day = 1;
 
         do{
             countInfected = population.count_infected();
-            cout << "In step " << day++ << " # sick = " << countInfected << ":" << population.toStringOne() << endl ;
+            //cout << "In step " << day++ << " # sick = " << countInfected << ":" << population.toStringOne() << endl ;
+            cout << "Infected count on day " << day << ": " << countInfected << endl;
+            day++;
             population.one_more_day();
         } while(countInfected>0);
     }
-    //exercise 49.5
+//exercise 49.5
     void neighborSimulation(int popSize, double probability){
         Disease disease(5, probability);  
         Population population(popSize,disease);
-        //population.initial_infect(disease); //infect only one person initially
         population.random_infectious(0.01, disease); //infect 10% of the population
         cout << "Initial values: \nDisease transmission probability: " << probability << endl;
         int countInfected =0;
         int countHealthy = 0;
         int day = 1;
-        vector <int> pop;
-        int ind = 0;
+        vector <int> pop; //stores indexes of infected values
+        int ind = 0; //index
         do {
             for(int x = 0; x<population.populationSize; x++){
                 if(population.people[x].get_status() == "Infected"){
-                    auto it = find(pop.begin(), pop.end(), x);
+                    auto it = find(pop.begin(), pop.end(), x); //checks if the index is not already in the vector
                     if (it == pop.end()) {
-                        pop.push_back(x); //checks if the index is not already in the vector
+                        pop.push_back(x); //if not, adds to vector
                     }
                 }
             } 
             countInfected = population.count_infected();
             countHealthy = population.count_healthy();
-            cout << "\nIn step " << day++ << " # sick = " << countInfected << "\n# healthy = " << countHealthy << endl;
+            //uncomment to see the value during EACH step!
+            //cout << "\nIn step " << day << " # sick = " << countInfected << "\n# healthy = " << countHealthy << endl;
+            day++;
             population.one_more_day();
-            while(ind<pop.size()){
+            while(ind<pop.size()){ //loops through infected indexes
                 population.neighbor(disease, probability, pop[ind]);    
                 ind++;    
             }
@@ -74,44 +74,46 @@ void personSimulation(){
    void vaccSimulation(int popSize, double probability, double vaccRate){ 
         Disease disease(5, probability);  
         Population population(popSize,disease);
-        //population.initial_infect(disease); //infect only one person initially
-        population.random_infectious(0.01, disease); //infect 10% of the population
-        population.random_vaccination(vaccRate);
+        population.random_infectious(0.01, disease); //infect 1% of the population
+        population.random_vaccination(vaccRate); //randomly vaccinates people based on vaccRate
         cout << "Initial values: \nDisease transmission probability: " << probability << endl;
         cout << "Vaccination Rate: " << vaccRate << endl;
         int countInfected =0;
         int countHealthy = 0;
         int countVaccinated = 0;
+        int countTotalInfected = 0;
         int day = 1;
-        vector <int> pop;
+        vector <int> pop; //vector of infected indexes
         int ind = 0;
         do {
             for(int x = 0; x<population.populationSize; x++){
                 if(population.people[x].get_status() == "Infected"){
-                    auto it = find(pop.begin(), pop.end(), x);
+                    auto it = find(pop.begin(), pop.end(), x); //checks if the index is not already in the vector
                     if (it == pop.end()) {
-                        pop.push_back(x); //checks if the index is not already in the vector
+                        pop.push_back(x); //if not, adds to vector
                     }
                 }
             } 
             countInfected = population.count_infected();
             countHealthy = population.count_healthy();
             countVaccinated = population.count_vaccinated();
-            cout << "\nIn step " << day++ << " # sick = " << countInfected << "\n# vaccinated = " << countVaccinated << "\n# healthy = " << countHealthy << endl;
+            countTotalInfected += population.count_infected(); 
+            //cout << "\nIn step " << day << " # sick = " << countInfected << "\n# vaccinated = " << countVaccinated << "\n# healthy = " << countHealthy << endl;
+            day++;
             population.one_more_day();
-            while(ind<pop.size()){
+            while(ind<pop.size()){ //loops through infected indexes
                 population.neighbor(disease, probability, pop[ind]);    
                 ind++;    
             }
         } while (countInfected > 0);
+        cout << "Number of infected people at the end: " << countTotalInfected << endl;
     }
 //exercise 49.7
     void testRandomContact(int popSize, double probability, double vaccRate){ 
-        Disease disease(5, probability);  // disease with 50% transmission chance and 5 days of sickness
+        Disease disease(5, probability); 
         Population population(popSize,disease);
-        //population.initial_infect(disease); //infect only one person initially
-        population.random_infectious(0.01, disease); //infect 10% of the population
-        population.random_vaccination(vaccRate);
+        population.random_infectious(0.01, disease); //infect 1% of the population
+        population.random_vaccination(vaccRate); //randomly vaccinates
         cout << "Initial values: \nDisease transmission probability: " << probability << endl;
         cout << "Vaccination Rate: " << vaccRate << endl;
         int countInfected = 0;
@@ -119,23 +121,24 @@ void personSimulation(){
         int countVaccinated = 0;
         int day = 1;
         int ind = 0;
-        vector <int> pop;
+        vector <int> pop; //vector of infected indexes
         do {
             for(int x = 0; x<population.populationSize; x++){
                 if(population.people[x].get_status() == "Infected"){
-                    auto it = find(pop.begin(), pop.end(), x);
+                    auto it = find(pop.begin(), pop.end(), x); //checks if the index is not already in the vector
                     if (it == pop.end()) {
-                        pop.push_back(x); //checks if the index is not already in the vector
+                        pop.push_back(x); 
                     }
                 }
             } 
             countInfected = population.count_infected();
             countHealthy = population.count_healthy();
             countVaccinated = population.count_vaccinated();
-            cout << "\nIn step " << day++ << " # sick = " << countInfected << "\n# vaccinated = " << countVaccinated << "\n# healthy = " << countHealthy << endl;
+            //cout << "\nIn step " << day << " # sick = " << countInfected << "\n# vaccinated = " << countVaccinated << "\n# healthy = " << countHealthy << endl;
+            day++;
             population.one_more_day();
             while(ind<pop.size()){
-                population.random_contact_infection(disease, 6, pop[ind]); 
+                population.random_contact_infection(disease, 6, pop[ind]); //random infection to a max of 6 people
                 ind++;  
             }  
         } while (countInfected > 0);
@@ -145,9 +148,8 @@ void personSimulation(){
     void herdImmunity(int popSize, double probability, double vaccRate){
         Disease disease(5, probability);  
         Population population(popSize,disease);
-        //population.initial_infect(disease); //infect only one person initially
         population.random_infectious(0.01, disease); //infect 1% of the population
-        population.random_vaccination(vaccRate);
+        population.random_vaccination(vaccRate); //vaccinates randomly
         cout << "Initial Values: \nDisease transmission probability: " << probability << endl;
         cout << "Vaccination Rate: " << vaccRate << endl;
         int countInfected = 0;
@@ -155,13 +157,13 @@ void personSimulation(){
         int countVaccinated = 0;
         int day = 1;
         int ind = 0;
-        vector <int> pop;
+        vector <int> pop; //vector of infected indexes
         do {
             for(int x = 0; x<population.populationSize; x++){
                 if(population.people[x].get_status() == "Infected"){
-                    auto it = find(pop.begin(), pop.end(), x);
+                    auto it = find(pop.begin(), pop.end(), x); //checks if the index is not already in the vector
                     if (it == pop.end()) {
-                        pop.push_back(x); //checks if the index is not already in the vector
+                        pop.push_back(x); 
                     }
                 }
             } 
@@ -171,7 +173,7 @@ void personSimulation(){
             day++;
             population.one_more_day();
             while(ind<pop.size()){
-                population.random_contact_infection(disease, 6, pop[ind]); 
+                population.random_contact_infection(disease, 6, pop[ind]); //randomly touches max of 6 people
                 ind++;  
             }  
         } while (countInfected > 0);
@@ -183,11 +185,20 @@ void personSimulation(){
 
 
 int main(){
-    //personSimulation(); //exercise 49.1
-    //populationSimulation(10000,0.30); //exercise 49.4
-    //neighborSimulation(10000,1.0); //exercise 49.5
-    vaccSimulation(10000,0.1,0.75); //exercise 49.6
-    //testRandomContact(10000,0.30,0.75); //exercise 49.7
+    //uncomment based on the excercise you would like to run!
+    //for running purposes, I will leave the testRandomContact excercise unccomented!
+    cout << "Person Simulation:\n";
+    personSimulation(); //exercise 49.1
+    cout << "--------------------------\nPopulation Simulation:\n";
+    populationSimulation(10000,0.30); //exercise 49.4
+    cout << "--------------------------\nNeighbor Simulation:\n";
+    neighborSimulation(10000,0.5); //exercise 49.5
+    cout << "--------------------------\nVaccination Simulation:\n";
+    vaccSimulation(10000,0.3,0.75); //exercise 49.6
+    cout << "--------------------------\nRandom Contact Simulation:\n";
+    testRandomContact(10000,0.3,0.75); //exercise 49.7
+    cout << "--------------------------\nHerd Immunity Simulation:\n";
+    herdImmunity(10000,0.3,0.75);
 /*
     for(double x = 0.10; x<=1; x+=0.20){ //exercise 49.8
         for(double y = 0.10; y<=0.90; y+=0.10){
@@ -196,6 +207,5 @@ int main(){
         }
     } 
 */
-
     return 0;
 }
